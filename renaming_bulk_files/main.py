50b60317a07011e51e36232b053cf9cf40657384
Dr-Phi <eliasrojaslon@gmail.com>
2022-08-12 21:35:22 -0500

    more readable

--- renaming_bulk_files/main.py
+++ renaming_bulk_files/main.py
@@ -1,14 +1,14 @@
 # This can go into a folder and rename all the files to whatever we give it
-import os
+import os # this allow us to access to our Operating System
 
 def main():
     i = 0
-    path = "/home/el_as/Pictures/icons_webdev/"
+    path = "/home/el_as/Pictures/icons_webdev/" # Our target dir
     for filename in os.listdir(path):
-        my_dest = "icon" + str(i) + ".png"
+        new_name = "icon" + str(i) + ".png"
         my_source = path + filename
-        my_dest = path + my_dest
-        os.rename(my_source, my_dest)
+        new_name = path + new_name
+        os.rename(my_source, new_name)
         i += 1
 
 if __name__ == "__main__":
